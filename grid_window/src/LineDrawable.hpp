3f43b9aac85acaf01593c0ef3406853751441ccd
shtseng <q253upng@gmail.com>
2020-01-22 01:10:54 +0800

    Add simple test path and make object to follow(bugs)

--- grid_window/src/LineDrawable.hpp
+++ grid_window/src/LineDrawable.hpp
@@ -10,10 +10,12 @@ using Object3D = SceneGraph::Object<SceneGraph::MatrixTransformation3D>;
 class LineDrawable : public Magnum::SceneGraph::Drawable3D
 {
     public:
-        explicit LineDrawable(Object3D& object, Shaders::Flat3D& shader, SceneGraph::DrawableGroup3D* drawables)
+        explicit LineDrawable(Object3D& object, Shaders::Flat3D& shader, SceneGraph::DrawableGroup3D* drawables,
+            Vector3&& start, Vector3&& end)
             : SceneGraph::Drawable3D{object, drawables}
             , _shader(shader){
-            _mesh = MeshTools::compile(Primitives::line3D({0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}));
+            // _mesh = MeshTools::compile(Primitives::line3D({0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}));
+            _mesh = MeshTools::compile(Primitives::line3D(start, end));
         }
 
         virtual ~LineDrawable() = default;
